7532a13e14dce6ed1a86b26ca8913bc330c42fd7
George Chang <changg@gmail.com>
2018-10-01 18:01:49 -0400

    fixes for imports

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -1,10 +1,12 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.config');
+const path = require('path');
 
 module.exports = merge(common, {
     mode: 'production',
     devtool: 'source-map',
     output: {
-        filename: '[name].min.js'
+        path: path.resolve(__dirname, ".."),
+        filename: 'index.js'
     }
 });
